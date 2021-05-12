b3953b77007beab2bf670ffb6316efa8882914e8
DevarajSekar <devarajecengineering@gmail.com>
2021-05-12 21:02:22 +0530

    Added popup sample

---
+++ NativePopup/NativePopup/NativePopup/PopupPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NativePopup
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyPopupPage : Popup
+    {
+        public MyPopupPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Dismiss(null);
+        }
+    }
+}
