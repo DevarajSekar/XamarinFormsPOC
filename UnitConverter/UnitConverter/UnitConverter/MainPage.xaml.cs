5952655e97e01c9c009708bfd4a7a2a5ee73c0a2
DevarajSekar <devarajecengineering@gmail.com>
2021-05-13 10:38:20 +0530

    Added unit converter sample.

---
+++ UnitConverter/UnitConverter/UnitConverter/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace UnitConverter
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            label.Text = UnitConverters.CelsiusToFahrenheit(Convert.ToDouble(entry.Text)).ToString();
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            label.Text = UnitConverters.KilometersToMiles(Convert.ToDouble(entry.Text)).ToString();
+        }
+    }
+}
