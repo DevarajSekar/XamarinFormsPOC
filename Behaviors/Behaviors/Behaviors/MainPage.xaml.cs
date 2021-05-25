5b51299114e6fecc6dd1eeb0fd854c903c838cc9
DevarajSekar <devarajecengineering@gmail.com>
2021-05-25 21:03:41 +0530

    Added Behavior sample.

---
+++ Behaviors/Behaviors/Behaviors/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Behaviors
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class ModelClass: BaseViewModel
+    {
+        private int count;
+        private string item;
+
+        public ICommand ControlCommand { get; set; }
+
+        public ModelClass()
+        {
+            ControlCommand = new Command<string>((s) => UpdateCount(s));
+        }
+
+        void UpdateCount(string s)
+        {
+            Count++;
+            Item = s;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+            set
+            {
+                count = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Item
+        {
+            get
+            {
+                return item;
+            }
+            set
+            {
+                item = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Base view model for entire project. 
+    /// </summary>
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
