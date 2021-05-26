b15ef861e384578b3bb7339c0192c8d45323b49c
DevarajSekar <devarajecengineering@gmail.com>
2021-05-26 16:25:04 +0530

    Added BarCode sample

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScanner/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BarcodeScanner
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ZXingScannerView_OnScanResult(ZXing.Result result)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                scanner.IsScanning = false;
+                resultLabel.Text = "Info : " + result.Text + "\nBarcode Type : " +
+                    result.BarcodeFormat.ToString();
+            });
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            scanner.IsScanning = true;
+        }
+    }
+}
