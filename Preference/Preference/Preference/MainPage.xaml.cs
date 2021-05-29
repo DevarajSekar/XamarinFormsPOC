10854e446315a7f3e8286d0ff5f7dc72757f21f9
DevarajSekar <devarajecengineering@gmail.com>
2021-05-29 17:13:13 +0530

    Added Preference sample

---
+++ Preference/Preference/Preference/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace Preference
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            entry.Text = Preferences.Get("entry", string.Empty);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Preferences.Set("entry", entry.Text);
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Preferences.Clear();
+        }
+    }
+}
