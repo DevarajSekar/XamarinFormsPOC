b0f1f9525d154a10ae9617deb25b24f8ae4bf170
DevarajSekar <devarajecengineering@gmail.com>
2021-06-01 11:22:16 +0530

    Added Value converter sample.

---
+++ ValueConverter/ValueConverter/ValueConverter/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ValueConverter
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class ModelClass : INotifyPropertyChanged
+    {
+        public int Color { get; set; } = 1;
+
+        public ObservableCollection<GenderClass> MyList { get; set; } = new ObservableCollection<GenderClass>()
+        {
+             new GenderClass(){ Name="Male", MyGender= Gender.Male },
+             new GenderClass(){ Name="Female", MyGender= Gender.Female },
+             new GenderClass(){ Name="Male", MyGender= Gender.Male },
+             new GenderClass(){ Name="Female", MyGender= Gender.Female },
+             new GenderClass(){ Name="Male", MyGender= Gender.Male },
+        };
+
+        public Gender Gender { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public class GenderClass
+    {
+        public Gender MyGender { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+    public class ColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return Color.LightBlue;
+
+            var result = value;
+
+            switch (result)
+            {
+                case (Gender.Male):
+                    return Color.LightBlue;
+                default:
+                    return Color.LightPink;
+            }
+                
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
